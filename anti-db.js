a2e5d3a8e0407c7e8a15721279ef348d820d5c9f
digital labs <success@dpsw.info>
2013-07-09 18:46:06 -0500

    Update anti-db.js

--- anti-db.js
+++ anti-db.js
@@ -1,17 +1,20 @@
-var funcs = [], fs = require('fs');
-
-global._require = function (fn, isArray, safer){
-
-  var ob = fs.existsSync(fn) ? JSON.parse(fs.readFileSync(fn).toString()) : (isArray ? [] : {});
-	funcs.push([fn, ob]);
-	var fin = function(){
-		funcs.map(function(arr){ fs.writeFileSync(arr[0], JSON.stringify(arr[1], null, 4)) });
+module.exports = function(){
+	
+	var funcs = [], fs = require('fs');
+	
+	global._require = function (fn, isArray, safer){
+	
+	  var ob = fs.existsSync(fn) ? JSON.parse(fs.readFileSync(fn).toString()) : (isArray ? [] : {});
+		funcs.push([fn, ob]);
+		var fin = function(){
+			funcs.map(function(arr){ fs.writeFileSync(arr[0], JSON.stringify(arr[1], null, 4)) });
+		}
+	
+		if(safer) setInterval(fin, safer);
+	
+		process.on('SIGINT', fin);
+		process.on('exit', fin);
+	
+		return ob;
 	}
-
-	if(safer) setInterval(fin, safer);
-
-	process.on('SIGINT', fin);
-	process.on('exit', fin);
-
-	return ob;
 }
